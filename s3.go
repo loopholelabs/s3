46e149f6e36f89e94432f761f1a69fa86ff322de
Shivansh Vij <shivanshvij@loopholelabs.io>
2023-07-17 18:08:49 -0700

    Renaming bucket variable to prefix in functions

--- s3.go
+++ s3.go
@@ -81,28 +81,28 @@ func New(options *Options, logger *zerolog.Logger) (*Client, error) {
 	return e, nil
 }
 
-func (e *Client) PresignedGetObject(ctx context.Context, bucket string, key string, expires time.Duration) (*url.URL, error) {
-	objName := objectName(bucket, key)
+func (e *Client) PresignedGetObject(ctx context.Context, prefix string, key string, expires time.Duration) (*url.URL, error) {
+	objName := prefixedKey(prefix, key)
 	e.logger.Debug().Msgf("presigning object '%s' from bucket '%s' with expiry %s", objName, e.options.Bucket, expires)
 	return e.client.PresignedGetObject(ctx, e.options.Bucket, objName, expires, nil)
 }
 
-func (e *Client) GetObject(ctx context.Context, bucket string, key string) (io.ReadCloser, error) {
-	objName := objectName(bucket, key)
+func (e *Client) GetObject(ctx context.Context, prefix string, key string) (io.ReadCloser, error) {
+	objName := prefixedKey(prefix, key)
 	e.logger.Debug().Msgf("getting object '%s' from bucket '%s'", objName, e.options.Bucket)
 	return e.client.GetObject(ctx, e.options.Bucket, objName, e.getOpts)
 }
 
-func (e *Client) PutObject(ctx context.Context, bucket string, key string, reader io.Reader, objectSize int64, contentType string) (minio.UploadInfo, error) {
-	objName := objectName(bucket, key)
+func (e *Client) PutObject(ctx context.Context, prefix string, key string, reader io.Reader, objectSize int64, contentType string) (minio.UploadInfo, error) {
+	objName := prefixedKey(prefix, key)
 	e.logger.Debug().Msgf("putting object '%s' into bucket '%s'", objName, e.options.Bucket)
 	return e.client.PutObject(ctx, e.options.Bucket, objName, reader, objectSize, minio.PutObjectOptions{
 		ContentType: contentType,
 	})
 }
 
-func (e *Client) DeleteObject(ctx context.Context, bucket string, key string) error {
-	objName := objectName(bucket, key)
+func (e *Client) DeleteObject(ctx context.Context, prefix string, key string) error {
+	objName := prefixedKey(prefix, key)
 	e.logger.Debug().Msgf("deleting object '%s' from bucket '%s'", objName, e.options.Bucket)
 	return e.client.RemoveObject(ctx, e.options.Bucket, objName, e.removeOpts)
 }
@@ -124,6 +124,6 @@ func (e *Client) Close() error {
 	return nil
 }
 
-func objectName(bucket string, key string) string {
-	return fmt.Sprintf("%s/%s", bucket, key)
+func prefixedKey(prefix string, key string) string {
+	return fmt.Sprintf("%s/%s", prefix, key)
 }
